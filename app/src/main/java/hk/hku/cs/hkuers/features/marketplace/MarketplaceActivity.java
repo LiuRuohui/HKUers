30498ae867952b4862bcb0350374e5b42708fb04
de_jong_lin <de_jong_lin@outlook.com>
2025-04-19 10:22:27 +0800

    xml更新

--- app/src/main/java/hk/hku/cs/hkuers/features/marketplace/MarketplaceActivity.java
+++ app/src/main/java/hk/hku/cs/hkuers/features/marketplace/MarketplaceActivity.java
@@ -1,6 +1,19 @@
 package hk.hku.cs.hkuers.features.marketplace;
 
+import android.content.Intent;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import hk.hku.cs.hkuers.features.trade.TradeListActivity;
+
 public class MarketplaceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 直接跳转到交易列表页面
+        Intent intent = new Intent(this, TradeListActivity.class);
+        startActivity(intent);
+        finish(); // 结束当前Activity
+    }
 }
